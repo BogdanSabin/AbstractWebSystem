1f72f9bc48085d8dc982325f7b7f8d9c4a69f0f4
BogdanSabin <melciu.bogdan@yahoo.com>
2022-05-29 19:52:13 +0300

    Update routes for desktop and mobile servers

--- server/src/routes/DesktopRouter.ts
+++ server/src/routes/DesktopRouter.ts
@@ -4,12 +4,12 @@ import { RoutersFactory } from '.';
 
 export class DesktopRoutes {
     static create(app: express.Application, routers: RoutersFactory): express.Application {
-        app.use('/api/admin/auth', routers.getAuthRoutes().create());
-        app.use('/api/admin/site', routers.getSiteRoutes().create());
-        app.use('/api/admin/product', routers.getProductRoutes().create());
-        app.use('/api/admin/order', routers.getOrderRoutes().create());
-        app.use('/api/admin/theme', routers.getThemeRoutes().create());
-        app.use('/api/admin/image', routers.getImageRoutes().create());
+        app.use('/api/desktop/auth', routers.getAuthRoutes().create());
+        app.use('/api/desktop/site', routers.getSiteRoutes().create());
+        app.use('/api/desktop/product', routers.getProductRoutes().create());
+        app.use('/api/desktop/order', routers.getOrderRoutes().create());
+        app.use('/api/desktop/theme', routers.getThemeRoutes().create());
+        app.use('/api/desktop/image', routers.getImageRoutes().create());
 
         return app;
     }
--- server/src/routes/MobileRouter.ts
+++ server/src/routes/MobileRouter.ts
@@ -4,12 +4,12 @@ import { RoutersFactory } from '.';
 
 export class MobileRoutes {
     static create(app: express.Application, routers: RoutersFactory): express.Application {
-        app.use('/api/admin/auth', routers.getAuthRoutes().create());
-        app.use('/api/admin/site', routers.getSiteRoutes().create());
-        app.use('/api/admin/product', routers.getProductRoutes().create());
-        app.use('/api/admin/order', routers.getOrderRoutes().create());
-        app.use('/api/admin/theme', routers.getThemeRoutes().create());
-        app.use('/api/admin/image', routers.getImageRoutes().create());
+        app.use('/api/mobile/auth', routers.getAuthRoutes().create());
+        app.use('/api/mobile/site', routers.getSiteRoutes().create());
+        app.use('/api/mobile/product', routers.getProductRoutes().create());
+        app.use('/api/mobile/order', routers.getOrderRoutes().create());
+        app.use('/api/mobile/theme', routers.getThemeRoutes().create());
+        app.use('/api/mobile/image', routers.getImageRoutes().create());
 
         return app;
     }
